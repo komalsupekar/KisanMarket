8bc6e6454fbf0899b0abc57f77316342f344bedd
madhusudan babar <madhusudanbabar@gmail.com>
2019-11-16 13:04:59 +0530

    Added splashscreen

---
+++ app/src/main/java/com/komal/krypton/kisan/market/splash.java
@@ -0,0 +1,32 @@
+package com.komal.krypton.kisan.market;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                finish();
+                startActivity(i);
+            }
+        },2000);
+    }
+}
