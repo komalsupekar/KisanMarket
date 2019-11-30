37b1a0097b1d84a983fca3425606b2b58de244e2
madhusudan babar <madhusudanbabar@gmail.com>
2019-11-30 19:10:33 +0530

    30112019

--- app/src/main/java/com/komal/krypton/kisan/market/splash.java
+++ app/src/main/java/com/komal/krypton/kisan/market/splash.java
@@ -1,13 +1,12 @@
 package com.komal.krypton.kisan.market;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
 import android.view.WindowManager;
+
 
 public class splash extends AppCompatActivity {
 
@@ -15,7 +14,6 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
